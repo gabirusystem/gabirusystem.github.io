981959fd18ee98f9b3103dd66d76660f3c540327
gabiruartes3d-dev <gabiruartes3d-dev@users.noreply.github.com>
2026-08-14 13:14:48 -0300

    Publica GabiruSystem 1.2.0 offline-first

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "45177052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2503583523" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
